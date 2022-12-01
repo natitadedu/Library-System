91c083ba32f31af612def4c62d2575225afee452
natitadedu <111113618+natitadedu@users.noreply.github.com>
2022-12-01 19:18:59 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // object create -> className objectName = new className();
+
+
+        librarySystem books = new librarySystem();
+        books.addRenter(0,"Ayele");
+books.addBooks("Nomadic Ways of life");
+
+
+    }
+}
